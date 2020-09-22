57ccff0e5f9c91126665d1bafac869a53e9ebfe1
Brian Burkard <brianburkard@gmail.com>
2020-09-22 19:40:37 -0400

    Constructor Pattern

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const calc = () => {
-    return 4 * 3;
+class Car {
+    constructor(doors, engine, color){
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
 }
 
-let aNumber = calc();
-
-const printCalc = (callback) => {
-    console.log(callback());
+class Van extends Car {
+    constructor(doors, engine, color){
+        super(doors, engine, color);
+        this.wheels = 4;
+    }
 }
 
-printCalc(calc);
-
-//console.log(aNumber);
-
-
+const civic = new Car(4, 'V6', 'red');
+const caravan = new Van(4, 'V6', 'gray');
+console.log(civic);
+console.log(caravan);
