fafdc125384fbaeab943420b422d79afd550b02e
Brian Burkard <brianburkard@gmail.com>
2020-09-22 20:44:39 -0400

    Factory Pattern

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-let instance = null;
-
 class Car {
     constructor(doors, engine, color){
-        if(!instance){
-            this.doors = doors;
-            this.engine = engine;
-            this.color = color;
-            instance = this;
-        }else{
-            return instance;
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
+}
+
+class carFactory {
+    createCar(type){
+        switch(type) {
+            case 'civic':
+                return new Car(4, 'V6', 'red');
+            case 'cavalier':
+                return new Car(4, 'V6', 'gray');
         }
     }
 }
 
-// class Van extends Car {
-//     constructor(doors, engine, color){
-//         super(doors, engine, color);
-//         this.wheels = 4;
-//     }
-// }
 
-const civic = new Car(4, 'V6', 'red');
-const cavalier = new Car(4, 'V6', 'gray');
+const factory = new carFactory();
+
+const civic = factory.createCar('civic');
+const cavalier = factory.createCar('cavalier');
+
 console.log(civic);
 console.log(cavalier);
