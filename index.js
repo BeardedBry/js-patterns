07162a44eb9ac1bf3ef39d8e72a79fc862a548b2
Brian Burkard <brianburkard@gmail.com>
2020-09-22 23:00:20 -0400

    Abstract Factory Pattern

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class Car {
     }
 }
 
-class carFactory {
+class CarFactory {
     createCar(type){
         switch(type) {
             case 'civic':
@@ -17,11 +17,41 @@ class carFactory {
     }
 }
 
+class SUV {
+    constructor(doors, engine, color){
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
+}
 
-const factory = new carFactory();
+class SuvFactory {
+    createCar(type){
+        switch(type) {
+            case 'cx5':
+                return new Car(4, 'V6', 'red');
+            case 'santa fe':
+                return new Car(4, 'V6', 'gray');
+        }
+    }
+}
 
-const civic = factory.createCar('civic');
-const cavalier = factory.createCar('cavalier');
+
+const carFactory = new CarFactory();
+const suvFactory = new SuvFactory();
+
+// Abstract factory
+const autoManufacturer = (type, model) => {
+    switch(type){
+        case 'car':
+            return carFactory.createCar(model);
+        case 'suv':
+            return suvFactory.createCar(model);
+    }
+}
+
+const civic = autoManufacturer('car','civic')
+const santaFe = autoManufacturer('suv','santa fe');
 
 console.log(civic);
-console.log(cavalier);
+console.log(santaFe);
